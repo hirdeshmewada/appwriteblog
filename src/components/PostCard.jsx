6f87f313d45ee825eeab4bc8a5e13e110e8fa29b
hirdeshmewada <djhd221@gmail.com>
2024-07-17 01:12:34 +0530

    UPDATE IMAGE PREVIEEW

--- src/components/PostCard.jsx
+++ src/components/PostCard.jsx
@@ -1,24 +1,37 @@
-import React from 'react'
-import appwriteService from "../appwrite/config"
-import {Link} from 'react-router-dom'
+import React, { useEffect, useState } from 'react';
+import appwriteService from "../appwrite/config";
+import { Link } from 'react-router-dom';
 
-function PostCard({$id, title, featuredImage}) {
-    
-  return (
-    <Link to={`/post/${$id}`}>
-        <div className='w-full bg-gray-100 rounded-xl p-4'>
-            <div className='w-full justify-center mb-4'>
-                <img src={appwriteService.getFilePreview(featuredImage)} alt={title}
-                className='rounded-xl' />
+function PostCard({ $id, title, featuredImage }) {
+    const [previewUrl, setPreviewUrl] = useState('');
 
+    useEffect(() => {
+        const fetchPreviewUrl = async () => {
+            try {
+                const url = await appwriteService.getFilePreview(featuredImage);
+                setPreviewUrl(url);
+            } catch (error) {
+                console.error("Error fetching file preview:", error);
+            }
+        };
+
+        fetchPreviewUrl();
+    }, [featuredImage]);
+
+    return (
+        <Link to={`/post/${$id}`}>
+            <div className='w-full bg-gray-100 rounded-xl p-4'>
+                <div className='w-full justify-center mb-4'>
+                    {previewUrl ? (
+                        <img src={previewUrl} alt={title} className='rounded-xl' />
+                    ) : (
+                        <div>Loading...</div>
+                    )}
+                </div>
+                <h2 className='text-xl font-bold'>{title}</h2>
             </div>
-            <h2
-            className='text-xl font-bold'
-            >{title}</h2>
-        </div>
-    </Link>
-  )
+        </Link>
+    );
 }
 
-
-export default PostCard
+export default PostCard;
